2f96f47093bbdd8b0e16a1e511f536657e6f2faf
Sava-Bullion <84075070+Sava-Bullion@users.noreply.github.com>
2021-05-12 21:03:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,22 +40,3 @@ def say_welcome(message):
         '<b>Hello! Get your publishers stats  <a href="https://admin.bullionyield.com/publisher/login">Bullion.Yield</a></b>',
         parse_mode='html'
     )
-
-
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
-            bot.send_message(message.chat.id, random.choice(resp['out']))
-            return
-
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
-    bot.send_message(message.chat.id, message.text)
-
-
-if __name__ == '__main__':
-    if os.environ.get("IS_PRODUCTION", "False") == "True":
-        app.run()
-    else:
-        bot.infinity_polling()
