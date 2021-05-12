1838244126617eb8f1c67ea5bcefad1cdedefe6c
Sava-Bullion <84075070+Sava-Bullion@users.noreply.github.com>
2021-05-12 20:34:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ from webhook import app
 # --------------- dialog params -------------------
 dialog = {
     'hello': {
-        'in': ['привет', 'hello', 'hi', 'privet', 'hey'],
+        'in': ['hello', 'hi', 'privet', 'hey','Hello', 'Hi','Hey'],
+        'out': ['What`s up', 'Yo Nigga', 'Sup bro!', 'Hey! Me so horny!', 'GET OVER HERE!']
+    {
+    'привет': {
+        'in': ['привет', 'Здравствуйте', 'hi', 'privet', 'hey'],
         'out': ['Приветствую', 'Здравствуйте', 'Привет!']
     },
     'how r u': {
