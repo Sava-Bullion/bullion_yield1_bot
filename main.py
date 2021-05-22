21512fe1670555dbf5a76d99f7cb516c62fdaa08
Sava-Bullion <84075070+Sava-Bullion@users.noreply.github.com>
2021-05-22 15:59:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,9 @@ def say_welcome(message):
         message.chat.id,
         '<b>Hello! Wellcome to Bullion.Media <a href="https://bullion.media/">Bullion.Media.</a>Type /admin to get adnig login page</b>',
         parse_mode='html'
+    )
+    bot.send_message(
+        message.chat.id,
         message.chat.id,
         '<b>Type /admin to get adnig login page. Type /publisher to get publisher stats page. Type /advertiser to get advertiser stats page</b>',
         parse_mode='html'
